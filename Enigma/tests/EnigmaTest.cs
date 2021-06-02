24be35d8946dc41b6b4341556181a6a5efa4075d
Mikael Fredriksson <mikael.fredriksson@lakemedelsverket.se>
2021-06-02 09:40:13 +0200

    added unit test framework

---
+++ Enigma/tests/EnigmaTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace net.sictransit.crypto.enigma.tests
+{
+    [TestClass]
+    public class EnigmaTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
